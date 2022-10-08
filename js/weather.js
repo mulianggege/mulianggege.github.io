23d734802099955172fa70c2859f6d2fbc9ee8ca
mulianggege <mulianggege@gmail.com>
2022-10-08 21:26:03 +0800

    Site updated: 2022-10-08 21:26:02

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "000000",
+        "tmpSize": "16",
+        "cityColor": "9FC5E8",
+        "citySize": "16",
+        "aqiColor": "EA9999",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "zh",
+        "borderRadius": "5",
+        "fixed": "false",
+        "vertical": "center",
+        "horizontal": "center",
+        "key": "767a1dfa0d1a4a6a88df3d6ee2a90156"
+    }
+}
